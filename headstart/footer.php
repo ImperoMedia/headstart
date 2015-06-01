85d2017c685ed14851d5dd777fa58ac749a12095
Luke Kohler <lukekohler@imperomedia.com>
2015-06-01 00:11:22 -0700

    Initial Commit of _S Theme
    Underscores theme added, no changes made.

---
+++ headstart/footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package headstart
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'headstart' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'headstart' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'headstart' ), 'headstart', '<a href="http://imperomedia.com" rel="designer">Luke Kohler</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
