f0e4d09b62b736ab3c43770d9dfc5fb5a90ab321
Luke Kohler <lukekohler@imperomedia.com>
2015-08-11 02:53:26 -0700

    Added animate.css link
    Hard coded header link to animate.min.css file

--- headstart/header.php
+++ headstart/header.php
@@ -17,7 +17,7 @@
 
 <!--EXT STYLESHEET LINKS-->
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="http://assets.imperomedia.com/code/animate.css">
+<link rel="stylesheet" href="/css/animate.min.css">
 <!--/END STYLESHEET LINKS-->
 
 <?php wp_head(); ?>
